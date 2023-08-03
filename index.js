37a4380ac1768553220d6c42627f74c057160ce5
tsusowake <tsusowake@gmail.com>
2023-08-03 21:01:00 +0900

    confff

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ console.group("PROCESS_ENV");
 console.log("APP_ENV: ", process.env.APP_ENV);
 
 const configEnv = {
-  ...require(`./config/${process.env.APP_ENV || "local"}.json`),
+  local: {
+    ...require("./config/local.json"),
+  },
+  dev: {
+    ...require("./config/dev.json"),
+  },
+  stg: {
+    ...require("./config/stg.json"),
+  },
+  prod: {
+    ...require("./config/prod.json"),
+  },
 };
-console.log("configEnv: ", configEnv);
+const conf = configEnv[process.env.APP_ENV] ?? {};
+console.log("conf: ", conf);
 
 console.groupEnd();
